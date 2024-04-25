502f17c4e1f9ae0eb39c9fa9e0794dd59852508a
Anamika <anamika.nayan@gmail.com>
2024-04-25 09:31:36 +0400

    Added minor change in assertion

--- cypress/e2e/PracticePage.cy.ts
+++ cypress/e2e/PracticePage.cy.ts
@@ -1,57 +1,57 @@
-import { practicePg } from "./pages/practicePg";
+import { practicePg } from './pages/practicePg';
 
 const practice = new practicePg();
-const txtFilePath: string = "./cypress/fixtures/pic.jpeg";
-const pageTitle: string = "Practice Page";
+const txtFilePath: string = './cypress/fixtures/pic.jpeg';
+const pageTitle: string = 'Practice Page';
 
-describe("Practice Page Test", () => {
-  beforeEach(() => {
-    practice.openPracticePage();
-  });
+describe('Practice Page Test', () => {
+	beforeEach(() => {
+		practice.openPracticePage();
+	});
 
-  it("Verify the header text after page is opened", () => {
-    practice.verifyPracticePageText(pageTitle);
-  });
+	it('Verify the header text after page is opened', () => {
+		practice.verifyPracticePageText(pageTitle);
+	});
 
-  it("Verify Home button with the link", () => {
-    practice.verifyHomeBtn();
-  });
+	it('Verify Home button with the link', () => {
+		practice.verifyHomeBtn();
+	});
 
-  it("Verify dropdown", () => {
-    practice.selectDropdown();
-  });
+	it('Verify dropdown', () => {
+		practice.selectDropdown();
+	});
 
-  it("Verify image uploader", () => {
-    practice.uploadImg(txtFilePath);
-    practice.verifyImgDisplayed();
-  });
+	it('Verify image uploader', () => {
+		practice.uploadImg(txtFilePath);
+		practice.verifyImgDisplayed();
+	});
 
-  it("Verify and handle Open New Tab button", () => {
-    practice.verifyOpenNewTab();
-  });
+	it('Verify and handle Open New Tab button', () => {
+		practice.verifyOpenNewTab();
+	});
 
-  it("Verify alert", () => {
-    cy.task("readTxtFile", "./cypress/fixtures/alert-text.txt").then((text) => {
-      practice.invokeAlertText(text);
-      practice.clickAlertBtn();
-      practice.verifyAlertTxt(text);
-    });
-  });
+	it('Verify alert', () => {
+		cy.task('readTxtFile', './cypress/fixtures/alert-text.txt').then((text) => {
+			practice.invokeAlertText(text);
+			practice.clickAlertBtn();
+			practice.verifyAlertTxt();
+		});
+	});
 
-  it("Verify text box", () => {
-    practice.verifyTextBox(true);
-    practice.clickHideBtn();
-    practice.verifyTextBox(false);
-    practice.clickShowBtn();
-    practice.verifyTextBox(true);
-  });
+	it('Verify text box', () => {
+		practice.verifyTextBox(true);
+		practice.clickHideBtn();
+		practice.verifyTextBox(false);
+		practice.clickShowBtn();
+		practice.verifyTextBox(true);
+	});
 
-  it("Verify hover menu", () => {
-    practice.verifyHoverBtn();
-    practice.verifyHoverMenu();
-  });
+	it('Verify hover menu', () => {
+		practice.verifyHoverBtn();
+		practice.verifyHoverMenu();
+	});
 
-  it("Verify iframe visibility", () => {
-    practice.verifyIFrame();
-  });
+	it('Verify iframe visibility', () => {
+		practice.verifyIFrame();
+	});
 });
